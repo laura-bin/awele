f24411491c4ca7a840cdd9b61b6cda994bc5ffd5
Laura Bin <l.binacchi@gmail.com>
2020-06-23 10:22:54 +0200

    score model adapted to the cLiEnT demand

--- src/main/java/awele/model/Score.java
+++ src/main/java/awele/model/Score.java
@@ -1,13 +1,75 @@
 package awele.model;
 
-import java.util.Date;
+import java.sql.Timestamp;
 
+/**
+ * Score model used to log the game results in database
+ */
 public class Score {
 
     private int id;
-    private Date date;
-    private String playerName;
-    private int value;
-    private String gameLevel;
+    private Timestamp date;
+    private int duration;
+    private String humanPlayerName;
+    private String virtualPlayerName;
+    private int humanPlayerScore;
+    private int virtualPlayerScore;
+    private String winnerName;
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getHumanPlayerName() {
+        return humanPlayerName;
+    }
+
+    public void setHumanPlayerName(String humanPlayerName) {
+        this.humanPlayerName = humanPlayerName;
+    }
+
+    public String getVirtualPlayerName() {
+        return virtualPlayerName;
+    }
+
+    public void setVirtualPlayerName(String virtualPlayerName) {
+        this.virtualPlayerName = virtualPlayerName;
+    }
+
+    public int getHumanPlayerScore() {
+        return humanPlayerScore;
+    }
+
+    public void setHumanPlayerScore(int humanPlayerScore) {
+        this.humanPlayerScore = humanPlayerScore;
+    }
+
+    public int getVirtualPlayerScore() {
+        return virtualPlayerScore;
+    }
+
+    public void setVirtualPlayerScore(int virtualPlayerScore) {
+        this.virtualPlayerScore = virtualPlayerScore;
+    }
+
+    public String getWinnerName() {
+        return winnerName;
+    }
+
+    public void setWinnerName(String winnerName) {
+        this.winnerName = winnerName;
+    }
 
 }
